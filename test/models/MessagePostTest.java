174958bd27dc800d836a30f331fcee1ca207f55e
ScottM2312 <scottmitchell1202@gmail.com>
2026-04-03 09:36:56 +0100

    Completed TODO task, final push

--- test/models/MessagePostTest.java
+++ test/models/MessagePostTest.java
@@ -9,18 +9,21 @@ import static org.junit.jupiter.api.Assertions.*;
 class MessagePostTest {
     MessagePost messagePost1;
     MessagePost messagePost2;
+    MessagePost messagePost3;
 
 
     @BeforeEach
     void setUp() {
         messagePost1 = new MessagePost("Mary", "Hi there.");
         messagePost2 = new MessagePost("John", "I'm on my way.");
+        messagePost3 = new MessagePost("Scott", "I'm on my way to pick up the groceries from the shop, please wait for me.");
     }
 
     @AfterEach
     void tearDown() {
         messagePost1 = null;
         messagePost2 = null;
+        messagePost3 = null;
     }
 
     @Test
@@ -33,5 +36,9 @@ class MessagePostTest {
         assertEquals("Bye Bye", messagePost1.getMessage());
         assertEquals("Wait.. Wait..", messagePost2.getMessage());
         //TODO  test when try to set a message that's too long
+        assertEquals("I'm on my way to pick up the groceries from the shop, please wait for me.",messagePost3.getMessage());
+        messagePost3.setMessage("See you later");
+        assertEquals("See you later", messagePost3.getMessage());
+        //Test fails as the string is out of range set in messagePost class
     }
 }
